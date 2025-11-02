19436ad31f0a597f06d8c7699d82280a7cbd3aef
Egor Morozov <ienji.speaker@gmail.com>
2025-11-02 21:34:43 +0300

    Update ViewportFramebuffer

--- engine-editor/src/main/kotlin/com/craftware/editor/viewport/ViewportFramebuffer.kt
+++ engine-editor/src/main/kotlin/com/craftware/editor/viewport/ViewportFramebuffer.kt
@@ -27,15 +27,34 @@ class ViewportFramebuffer {
 
         colorTex = GL11.glGenTextures()
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, colorTex)
-        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, width, height, 0, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, 0)
+        GL11.glTexImage2D(
+            GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB,
+            width, height, 0, GL11.GL_RGB, GL11.GL_UNSIGNED_BYTE, 0L
+        )
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR)
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR)
-        GL30.glFramebufferTexture2D(GL30.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT0, GL11.GL_TEXTURE_2D, colorTex, 0)
+        GL30.glFramebufferTexture2D(
+            GL30.GL_FRAMEBUFFER,
+            GL30.GL_COLOR_ATTACHMENT0,
+            GL11.GL_TEXTURE_2D,
+            colorTex,
+            0
+        )
 
         depthRbo = GL30.glGenRenderbuffers()
         GL30.glBindRenderbuffer(GL30.GL_RENDERBUFFER, depthRbo)
         GL30.glRenderbufferStorage(GL30.GL_RENDERBUFFER, GL30.GL_DEPTH24_STENCIL8, width, height)
-        GL30.glFramebufferRenderbuffer(GL30.GL_FRAMEBUFFER, GL30.GL_DEPTH_STENCIL_ATTACHMENT, GL30.glGenRenderbuffers(), depthRbo)
+        GL30.glFramebufferRenderbuffer(
+            GL30.GL_FRAMEBUFFER,
+            GL30.GL_DEPTH_STENCIL_ATTACHMENT,
+            GL30.GL_RENDERBUFFER,
+            depthRbo
+        )
+
+        val status = GL30.glCheckFramebufferStatus(GL30.GL_FRAMEBUFFER)
+        if (status != GL30.GL_FRAMEBUFFER_COMPLETE) {
+            error("Framebuffer is not complete: $status")
+        }
 
         GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, 0)
     }
