fc39a7a8c31cc7e242279e12b390ce39f51edd41
Egor Morozov <ienji.speaker@gmail.com>
2025-11-08 02:04:26 +0300

    kge-editor: Add some kind of semi-bold to EditorFonts

--- kge-engine-editor/src/main/java/kge/editor/ui/EditorFont.kt
+++ kge-engine-editor/src/main/java/kge/editor/ui/EditorFont.kt
@@ -7,13 +7,15 @@ object EditorFont {
     lateinit var regular: ImFont
     lateinit var medium: ImFont
     lateinit var bold: ImFont
+    lateinit var semiBold: ImFont
     lateinit var italic: ImFont
 
     fun load(io: ImGuiIO) {
-        regular = io.fonts.addFontFromMemoryTTF(assetBytes("std/fonts/JetBrainsMono-Regular.ttf"), 16f)
-        medium = io.fonts.addFontFromMemoryTTF(assetBytes("std/fonts/JetBrainsMono-Medium.ttf"), 18f)
-        italic = io.fonts.addFontFromMemoryTTF(assetBytes("std/fonts/JetBrainsMono-Italic.ttf"), 16f)
-        bold = io.fonts.addFontFromMemoryTTF(assetBytes("std/fonts/JetBrainsMono-Bold.ttf"), 20f)
+        regular = io.fonts.addFontFromMemoryTTF(assetBytes("std/fonts/JetBrainsMono-Regular.ttf"), 18f)
+        medium = io.fonts.addFontFromMemoryTTF(assetBytes("std/fonts/JetBrainsMono-Medium.ttf"), 20f)
+        italic = io.fonts.addFontFromMemoryTTF(assetBytes("std/fonts/JetBrainsMono-Italic.ttf"), 18f)
+        bold = io.fonts.addFontFromMemoryTTF(assetBytes("std/fonts/JetBrainsMono-Bold.ttf"), 24f)
+        semiBold = io.fonts.addFontFromMemoryTTF(assetBytes("std/fonts/JetBrainsMono-Bold.ttf"), 21f)
     }
 
     fun assetBytes(assetPath: String): ByteArray {
