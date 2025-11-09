9051352125eca595a11a7dced6a0194f17efd172
Egor Morozov <ienji.speaker@gmail.com>
2025-11-09 15:39:26 +0300

    kge-editor: add Create option to EditorMenuBar.kt

--- kge-engine-editor/src/main/java/kge/editor/ui/EditorMenuBar.kt
+++ kge-engine-editor/src/main/java/kge/editor/ui/EditorMenuBar.kt
@@ -4,6 +4,8 @@ import imgui.ImGui
 import kge.api.editor.imgui.IRenderCallback
 import kge.api.std.IRenderable
 import kge.editor.EditorApplication
+import kge.editor.GameObject
+import kge.editor.component.MeshRenderer
 import org.lwjgl.glfw.GLFW
 
 class EditorMenuBar : IRenderable {
@@ -17,12 +19,30 @@ class EditorMenuBar : IRenderable {
                     }
                     ImGui.endMenu()
                 }
-                if (ImGui.menuItem("Exit")) {
-                    GLFW.glfwSetWindowShouldClose(EditorApplication.getInstance().getWindowHandle(), true)
-                }
                 ImGui.endMenu()
             }
 
+            val scene = EditorApplication.getInstance().getProjectManager().getCurrentScene()
+            if (scene != null) {
+                if (ImGui.beginMenu("Create")) {
+                    if (ImGui.menuItem("GameObject")) {
+                        val node = GameObject()
+                        scene.root.addChild(node)
+                        EditorApplication.getInstance().getEditorSelection().select(node)
+                    }
+                    if (ImGui.menuItem("Cube")) {
+                        val node = GameObject()
+                        val component = MeshRenderer()
+                        node.addComponent(component)
+                        scene.root.addChild(node)
+                        EditorApplication.getInstance().getEditorSelection().select(node)
+                    }
+                    ImGui.endMenu()
+                }
+            }
+            if (ImGui.menuItem("Exit")) {
+                GLFW.glfwSetWindowShouldClose(EditorApplication.getInstance().getWindowHandle(), true)
+            }
             ImGui.endMenuBar()
         }
     }
